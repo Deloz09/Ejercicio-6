a3aff5f4e018f9aab60bd6dc2fafcacff6b987e0
Deloz09 <84214640+Deloz09@users.noreply.github.com>
2021-11-09 13:11:43 -0600

    Update 1.4

--- Receipt.java
+++ Receipt.java
@@ -1,3 +1,22 @@
 public class Receipt {
-    
+    String client;
+    int nit;
+    String date;
+    int receiptNo;
+
+    public Receipt(String c, int n, String d, int rn){
+        this.client = c;
+        this.nit = n;
+        this.date = d;
+        this.receiptNo = rn;
+    }
+
+    public String getReceipt(){
+        String receipt = "";
+        String nitS = String.valueOf(this.nit);
+        String rnS = String.valueOf(this.receiptNo);
+        
+        receipt = client+","+nitS+","+date+","+rnS;
+        return receipt;
+    }
 }
