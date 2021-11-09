d9fa8543a30dc3acc05fcf80db33071810641a8a
gusanitor8 <gusanitor8@gmail.com>
2021-11-09 12:32:52 -0600

    Cambios 5

--- Controlador.java
+++ Controlador.java
@@ -1,7 +1,70 @@
-class Controlador{
+class Controlador{    
+
     public static void main(String[] args){
-        Smartphone smartphone = new Smartphone();
-        smartphone.toBrowse();    
+        menu();    
     
+    }
+
+
+    public static void menu(){
+        boolean condicion = true;
+        while(condicion){
+            switch(Vista.mainMenu()){
+                case 1:
+                    store();
+                    break;
+                case 2:
+                    break;
+                case 3: 
+                    condicion = false;
+                    break;
+            }
+        }
+    }
+
+    public static void store(){
+        boolean condicion = true;
+        while(condicion){
+            switch(Vista.displayStore()){
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3: 
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7: 
+                    break;
+                case 8:
+                    break;
+                case 9:
+                    break;
+                case 10:
+                    condicion = false;
+                    break;
+            }
+        }
+    }
+
+    public static void displayFunctionality(int option){
+        switch(option){
+            case 1: // Portable                
+                break;
+            case 2: // Reproducir Video
+                break;
+            case 3: // Fotografeable
+                break;
+            case 4: // Llamable
+                break;
+            case 5: // Navegable
+            
+
+        }
+
     }
 }
--- Vista.java
+++ Vista.java
@@ -4,6 +4,87 @@ import java.util.Scanner;
 public class Vista {
     static Scanner input = new Scanner(System.in);
 
+
+    /**
+     * Menu principal
+     * @return devuelve un entero que representa la opcion 
+     */
+    public static int mainMenu(){
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~ Menu Principal ~~~~~~~~~~~~~~~~~~~~~\n" +
+                            "1. Ver Productos \n" +
+                            "2. Ver Carrito \n" + 
+                            "3. Salir");
+        System.out.println("Ingrese opcion: ");
+
+        int opcion = 0;
+        try{
+            opcion = input.nextInt();
+            input.nextLine();
+
+            if((opcion > 3)||(opcion < 1)){
+                throw new IndexOutOfBoundsException();
+            }
+
+        }catch(IndexOutOfBoundsException e){
+            System.out.println("La opcion ingresada esta fuera de rango");
+            opcion = mainMenu();
+
+        }catch(InputMismatchException e){
+            input.nextLine();
+            System.out.println("El tipo de dato ingresado no es valido");
+            opcion = mainMenu();
+        }
+
+        return opcion;
+    }
+
+
+
+    public static int displayStore(){
+        System.out.println("~~~~~~~~~~~~~ Tienda ~~~~~~~~~~~~~ \n" +
+                            "1. Smartphone \n" +
+                            "2. Telefono Celular \n" +
+                            "3. Telefono Fijo \n" +
+                            "4. Camara \n" +
+                            "5. Desktop \n" +
+                            "6. Laptop \n" +
+                            "7. Smart TV \n" +
+                            "8. Tablet \n" +
+                            "9. Smartwatch \n" +
+                            "10. Salir");
+
+        int opcion = 0;
+        try{
+            System.out.println("Ingrese una opcion: ");
+            opcion = input.nextInt();
+            input.nextLine();
+
+            if ((opcion > 10) || (opcion < 1)){
+                throw new IndexOutOfBoundsException();                
+            }
+
+        }catch(IndexOutOfBoundsException e){
+            System.out.println("Opcion fuera del rango \n");
+            opcion = displayStore();
+
+        }catch(InputMismatchException e){
+            input.nextLine();
+            System.out.println("El tipo de dato que ingreso es incorrecto");
+            opcion = displayStore();
+        }
+
+        return opcion;
+    }
+
+    public static void addProductMenu(){
+        System.out.println("~~~~~~~~~~~~~~ Add Product Menu ~~~~~~~~~~~~~~ \n" +
+                            "1. Agregar Producto al Carrito \n" +
+                            "2. Salir \n" );
+    }
+
+    
+
+
     public static int askNum(int limit, String message){
         System.out.println(message);
 
@@ -32,6 +113,8 @@ public class Vista {
         return option;
     }
 
+
+
     public static String askString(String message){
         System.out.println(message);
 
@@ -47,7 +130,13 @@ public class Vista {
 
         return str;
     }
+   
     
+    public static void displayMessage(String message){
+        System.out.println(message);
+    }
+
+
     public static void calling(int num){
         System.out.println("llamando a: " + num);
     }
