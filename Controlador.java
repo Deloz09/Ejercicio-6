5dae1df8d267829f9dd36ebee12d5dc0c72cd954
gusanitor8 <gusanitor8@gmail.com>
2021-11-09 20:36:59 -0600

    Update Controlador.java

--- Controlador.java
+++ Controlador.java
@@ -2,9 +2,9 @@ import java.util.ArrayList;
 
 class Controlador{    
 
-    public static void main(String[] args){
-       // menu();    
+    public static void main(String[] args){       
         init();
+        menu();
     }
 
     public static void init(){
@@ -13,36 +13,35 @@ class Controlador{
         Product.setCounter(Integer.parseInt(data.get(0)[0]));
 
         for(int i = 1; i < data.size(); i++){
-            if(data.get(i)[5].equals("smartphone")){
+            if(data.get(i)[0].equals("smartphone")){
                 Smartphone smartPhone = new Smartphone(data.get(i));
-                Cart.addProduct(smartPhone);
-                System.out.println(smartPhone.objectToString());
+                Cart.addProduct(smartPhone);                
 
-            }else if(data.get(i)[5].equals("cellphone")){
+            }else if(data.get(i)[0].equals("cellphone")){
                 CellPhone cellPhone = new CellPhone(data.get(i));
                 Cart.addProduct(cellPhone);
 
-            }else if(data.get(i)[5].equals("linephone")){
+            }else if(data.get(i)[0].equals("linephone")){
                 LinePhone linePhone = new LinePhone(data.get(i));
                 Cart.addProduct(linePhone);
 
-            }else if(data.get(i)[5].equals("camera")){
+            }else if(data.get(i)[0].equals("camera")){
                 Camera camera = new Camera(data.get(i));
                 Cart.addProduct(camera);
 
-            }else if(data.get(i)[5].equals("laptop")){
+            }else if(data.get(i)[0].equals("laptop")){
                 Laptop laptop = new Laptop(data.get(i));
                 Cart.addProduct(laptop);
 
-            }else if(data.get(i)[5].equals("smartTV")){
+            }else if(data.get(i)[0].equals("smartTV")){
                 SmartTV smartTV = new SmartTV(data.get(i));
                 Cart.addProduct(smartTV);
 
-            }else if(data.get(i)[5].equals("Tablet")){
+            }else if(data.get(i)[0].equals("Tablet")){
                 Tablet tablet = new Tablet(data.get(i));
                 Cart.addProduct(tablet);
 
-            }else if(data.get(i)[5].equals("smartwatch")){
+            }else if(data.get(i)[0].equals("smartwatch")){
                 Smartwatch smartwatch = new Smartwatch(data.get(i));
                 Cart.addProduct(smartwatch);
 
@@ -87,25 +86,43 @@ class Controlador{
         while(condicion){
             switch(Vista.displayStore()){
                 case 1:
+                    Smartphone smartphone = new Smartphone();
                     break;
+
                 case 2:
+                    CellPhone cellphone = new CellPhone();
                     break;
+
                 case 3: 
+                    LinePhone linePhone = new LinePhone();
                     break;
+
                 case 4:
+                    Camera camera = new Camera();
                     break;
+
                 case 5:
+                    Desktop desktop = new Desktop();
                     break;
+
                 case 6:
+                    Laptop laptop = new Laptop();
                     break;
+
                 case 7: 
+                    SmartTV smartTV = new SmartTV();
                     break;
+
                 case 8:
+                    Tablet tablet =  new Tablet();
                     break;
+
                 case 9:
+                    Smartwatch smartwatch = new Smartwatch();
                     break;
+
                 case 10:
-                    condicion = false;
+                    condicion = false;                    
                     break;
             }
         }
@@ -113,7 +130,7 @@ class Controlador{
 
     public static void displayFunctionality(int option){
         switch(option){
-            case 1: // Portable                
+            case 1:                 
                 break;
             case 2: // Reproducir Video
                 break;
